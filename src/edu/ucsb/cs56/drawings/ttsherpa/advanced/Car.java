60aab3c830cdde7cfc9679b4c23ef23345c0937c
Tenzing Sherpa <ttsherpa@umail.ucsb.edu>
2016-07-23 22:15:41 -0700

    added in car.java and carwithwindowsandant

---
+++ src/edu/ucsb/cs56/drawings/ttsherpa/advanced/Car.java
@@ -0,0 +1,55 @@
+package edu.ucsb.cs56.drawings.ttsherpa.advanced;
+import java.awt.geom.GeneralPath;
+import java.awt.Shape;
+
+import java.awt.geom.Line2D;
+import java.awt.geom.Rectangle2D;
+import java.awt.geom.Ellipse2D;
+
+import edu.ucsb.cs56.drawings.utilities.ShapeTransforms;
+import edu.ucsb.cs56.drawings.utilities.GeneralPathWrapper;
+
+public class Car extends GeneralPathWrapper implements Shape
+{
+
+    public Car(double x, double y, double length, double height)
+    {
+
+		double trunkHeight= .53 * height;
+		double roofHeight= height - trunkHeight;
+		double lightheight= .20 * height;
+		double lightlength= .10 * length;
+	
+		Rectangle2D.Double base = 
+		new Rectangle2D.Double(x, y, length, 
+		trunkHeight);
+		
+		Rectangle2D.Double top=
+		new Rectangle2D.Double(x+(.25*length),y, 
+		length-(.5*length), height-trunkHeight);
+		
+		Shape upwards = ShapeTransforms.verticallyFlippedCopyOf(top);
+
+		Ellipse2D.Double backTire= new Ellipse2D.Double(x+30 , y+30, 33, 33);
+		Ellipse2D.Double frontTire= new Ellipse2D.Double(x+150 , y+30, 33, 33);
+		Rectangle2D.Double backlight =
+		    new Rectangle2D.Double(x, y, lightlength,
+					   lightheight);
+
+		Ellipse2D.Double frontlight =
+		    new Ellipse2D.Double(x+200, y, 10,20 );
+
+		Line2D.Double doorsep =
+		    new Line2D.Double (x+length/2, y,
+				       x+length/2, y+trunkHeight);
+		
+		GeneralPath wholeCar= this.get();
+		wholeCar.append(base, false);
+		wholeCar.append(frontTire, false);
+		wholeCar.append(backTire, false);
+		wholeCar.append(upwards, false);
+		wholeCar.append(backlight,false);
+		wholeCar.append(frontlight,false);
+		wholeCar.append(doorsep, false);
+    }
+}	
